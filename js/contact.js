0aab00a9b926623ced54b20d08a8b517225870b3
gabol2 <90797014+gabol2@users.noreply.github.com>
2022-08-11 21:08:03 -0600

    añadí un marker en los headquarters

--- js/contact.js
+++ js/contact.js
@@ -1,16 +1,27 @@
 let map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: {
-      lat: 10.007025797993402, 
-      lng:  -84.21644633461605,
-    },
-    zoom: 15,
-  });
+function initMap()
+{
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+            lat: 10.005026407041958,
+            lng: -84.21840218910089
+        },
+        zoom: 14
+    });
+
+    new google.maps.Marker({
+        position: {
+            lat: 10.005026407041958,
+            lng: -84.21840218910089
+        },
+        map,
+        title: "T3AMPO Headquarters",
+    });
 }
 
 $("#submit").click(
-    function() {
+    function ()
+    {
         alert("pain")
     }
 )
