e3d315e5e3df578d666896160246af6784d5f746
gabol2 <90797014+gabol2@users.noreply.github.com>
2022-08-11 20:39:36 -0600

    arreglos pagina contacto el link del archivo al javascript estaba mal escrito y no se encontraba el documento, la función tenía errores de sintáxis, añadí un mapa usando la API de google maps

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,16 @@
-${"#submit"}.click(
-    function() {
+let map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: {
+      lat: 10.007025797993402, 
+      lng:  -84.21644633461605,
+    },
+    zoom: 15,
+  });
+}
 
+$("#submit").click(
+    function() {
+        alert("pain")
     }
 )
