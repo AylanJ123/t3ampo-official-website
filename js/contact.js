334c2a781a96e7a3d8f41e11ee3bfad09d91ef51
AylanJ123 <aylanjosue@gmail.com>
2022-08-13 16:51:22 -0600

    Finished Jquery and JS

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,10 @@
-let map;
+function isValidEmail(email) {
+    return /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/
+    .test(email);
+}
+
+var map
+
 function initMap()
 {
     map = new google.maps.Map(document.getElementById("map"), {
@@ -20,8 +26,17 @@ function initMap()
 }
 
 $("#submit").click(
-    function ()
-    {
-        alert("pain")
+    function() {
+        let email = $("#email");
+        if (!isValidEmail(email.val())) email[0].setCustomValidity("Write a valid email");
+        else email[0].setCustomValidity("");
+    }
+)
+
+$("#form").submit(
+    function(e) {
+        e.preventDefault();
+        alert("The email has been sent");
+        location.reload()
     }
 )
